cd7d5056bbf33ffa2456249991e6cbb5230622b4
Jacob Goren <jdgoren@gmail.com>
2023-08-13 14:35:05 -0400

    cyan

--- src/store/accountAbstractionContext.tsx
+++ src/store/accountAbstractionContext.tsx
@@ -97,7 +97,7 @@ const AccountAbstractionProvider = ({ children }: { children: JSX.Element }) => 
     try {
       const options: Web3AuthOptions = {
         clientId: process.env.REACT_APP_WEB3AUTH_CLIENT_ID || '',
-        web3AuthNetwork: 'mainnet',
+        web3AuthNetwork: 'cyan',
         chainConfig: {
           chainNamespace: CHAIN_NAMESPACES.EIP155,
           chainId: chain.id,
